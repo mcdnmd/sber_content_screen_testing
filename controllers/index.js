8bf6d083d219f120845a10d3f8b5deb7cdffee6e
mcdnmd <kpoltoradnev@gmail.com>
2021-04-24 20:48:10 +0300

    simple mvp

---
+++ controllers/index.js
@@ -0,0 +1,25 @@
+const Site = require('../models/Site');
+
+module.exports.getCards = async (req, res) => {
+    //const candidate = await Site.findOne({});
+    let candidate = [
+        {
+            'name': 'sber.ru',
+            'text': 'Ну мы чакатон проводим',
+            'img': 'https://im0-tub-ru.yandex.net/i?id=6b09bcb8040700cb54a2c0ee2897d1fe&n=13',
+            'link': 'localhost:3000/'
+        },
+        {
+            'name': 'kontur.ru',
+            'text': 'Ну мы документы вертим',
+            'img': 'https://im0-tub-ru.yandex.net/i?id=aecc5bbe20a064a1524983d324bcc486&n=13',
+            'link': 'https://vk.com'
+        },
+        {
+            'name': 'ya.ru',
+            'text': 'Ну мы второй курс на стажировки не берем',
+            'img': 'https://im0-tub-ru.yandex.net/i?id=b34ca6de9776827a3612dc47015388d8&n=13',
+            'link': 'https://skb.kontur.ru'
+        }]
+    res.render('sites', { title: 'Библиотека', active: 'sites', sites: candidate})
+};
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/index')
+
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Главная', active: 'main' });
 });
 
-router.get('/sites', function(req, res, next) {
-  let site_list = [
-      {'name': 'sber.ru', 'text': 'Ну мы чакатон проводим', 'img': 'https://im0-tub-ru.yandex.net/i?id=6b09bcb8040700cb54a2c0ee2897d1fe&n=13'},
-    {'name': 'kontur.ru', 'text': 'Ну мы документы вертим', 'img': 'https://im0-tub-ru.yandex.net/i?id=aecc5bbe20a064a1524983d324bcc486&n=13'},
-    {'name': 'ya.ru', 'text': 'Ну мы второй курс на стажировки не берем', 'img': 'https://im0-tub-ru.yandex.net/i?id=b34ca6de9776827a3612dc47015388d8&n=13'}]
-  res.render('sites', { title: 'Библиотека', active: 'sites', sites: site_list});
-});
+router.get('/sites', controller.getCards);
 
 router.get('/site_state', function(req, res, next) {
-  res.render('site_state', { title: 'Библиотека', active: 'site state', sitename: 'state' });
+  res.render('site_state', {
+    title: 'Библиотека',
+    active: 'site state',
+    sitename: 'state',
+    link: req.body['site-link'] });
 });
 
 router.get('/login', function (req, res, next){
   res.render('login', { title: 'Авторизация', active: 'main'});
-})
+});
+
+router.get('/test_site/test_site', function(req, res, next) {
+  res.render('test_site/test_site', { title: 'Библиотека', active: 'test example', sitename: 'test' });
+});
+
+router.get('/test_site/test_site_change', function(req, res, next) {
+  res.render('test_site/test_site_change', { title: 'Библиотека', active: 'test example changed', sitename: 'test' });
+});
+
 
 module.exports = router;
