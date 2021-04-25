121b0a2eb7d00b02089fa61234a2e9af725f252b
mcdnmd <kpoltoradnev@gmail.com>
2021-04-25 13:24:33 +0300

    register done

--- middleware/passport.js
+++ middleware/passport.js
@@ -6,8 +6,15 @@ const keys = require('../config/keys');
 
 let cookieExtractor = function(req) {
     let token = null;
-    if (req && req.headers['cookie'])
-        token = req.headers['cookie'].slice(4);
+    if (req && req.headers['cookie']) {
+        let cookies = req.headers['cookie'].split('; ');
+        for(let i = 0; i < cookies.length; i++){
+            console.log(cookies[i])
+            let tmp = cookies[i].split("=");
+            if (tmp[0]==='jwt')
+                token = tmp[1];
+        }
+    }
     console.log(token)
     return token;
 };
